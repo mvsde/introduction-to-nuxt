24fe5acde2a193c05756e6c6962998503b6b9fad
Fynn Becker <info@fynn-becker.de>
2018-12-29 08:26:07 +0100

    chore: Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case']
   }
 }
