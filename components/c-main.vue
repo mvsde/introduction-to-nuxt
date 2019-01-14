752bbe8e56395e5351a117e5bec0fe32f64ac073
Fynn Becker <info@fynn-becker.de>
2019-01-14 15:22:43 +0100

    fix: Move global listeners back to layout
    This fixes a problem with constantly re-rendering the c-main component during view change.

--- components/c-main.vue
+++ components/c-main.vue
@@ -1,15 +1,5 @@
 <template>
-  <main
-    class="main"
-    :class="{ 'autohide': autohideEnabled }"
-  >
-    <global-events
-      @keyup.left="previousSlide"
-      @keyup.right="nextSlide"
-      @mousemove="autohide"
-      @mousedown="autohide"
-    />
-
+  <main class="main">
     <div
       v-if="$slots.header"
       class="header"
@@ -23,92 +13,15 @@
     >
       <slot />
     </div>
-
-    <transition name="fade">
-      <c-nav
-        v-show="!autohideEnabled"
-        :previous="previous"
-        :next="next"
-      />
-    </transition>
   </main>
 </template>
 
 <script>
-import CNav from '@/components/c-nav'
-import GlobalEvents from 'vue-global-events'
-
-import config from '@/config'
-
 export default {
-  components: {
-    CNav,
-    GlobalEvents
-  },
-
   props: {
     center: {
       type: Boolean,
       default: false
-    }
-  },
-
-  data () {
-    return {
-      autohideEnabled: true,
-      autohideTimer: null
-    }
-  },
-
-  computed: {
-    routeIndex () {
-      return config.sequence.indexOf(this.$route.path)
-    },
-    previous () {
-      if (this.routeIndex === 0) {
-        return
-      }
-
-      return config.sequence[this.routeIndex - 1]
-    },
-    next () {
-      if (this.routeIndex === config.sequence.length - 1) {
-        return
-      }
-
-      return config.sequence[this.routeIndex + 1]
-    }
-  },
-
-  created () {
-    this.autohide()
-  },
-
-  methods: {
-    autohide () {
-      if (this.autohideTimer) {
-        clearTimeout(this.autohideTimer)
-      }
-
-      this.autohideEnabled = false
-      this.autohideTimer = setTimeout(() => {
-        this.autohideEnabled = true
-        this.autohideTimer = null
-      }, 2000)
-    },
-    previousSlide () {
-      if (!this.previous) {
-        return
-      }
-
-      this.$router.push(this.previous)
-    },
-    nextSlide () {
-      if (!this.next) {
-        return
-      }
-
-      this.$router.push(this.next)
     }
   }
 }
@@ -122,10 +35,6 @@ export default {
   padding: 4vmin 5vmin;
 
   border: var(--frame-width) solid rgb(var(--color-light));
-}
-
-.autohide {
-  cursor: none;
 }
 
 .header {
